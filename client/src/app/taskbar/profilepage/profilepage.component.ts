86d5cff5ce45cd5a8db9106c47059e3bd98eee32
V1ct0rC <vgc3@cin.ufpe.br>
2023-03-09 17:47:31 -0300

    Estrutura da homepage para usuários não logados e continuação da implementação do perfil

--- client/src/app/taskbar/profilepage/profilepage.component.ts
+++ client/src/app/taskbar/profilepage/profilepage.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./profilepage.component.scss']
 })
 export class ProfilepageComponent {
+	username : string;
+	followers : Number;
 
+	constructor(){
+		// Change to default values
+		this.username = "Victor"; 
+		this.followers = 5471;
+	}
+
+	logOut(){
+		//To do
+	}
 }
--- client/src/app/taskbar/taskbar.component.ts
+++ client/src/app/taskbar/taskbar.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./taskbar.component.scss']
 })
 export class TaskbarComponent {
-
+	toHome() : void{
+		//To do
+	}
+	
+    showProfile() : void{
+		let disp : string = document.getElementById("pp")!.style.display;
+		
+        if(disp != "none"){
+          document.getElementById("pp")!.style.display="none";
+        }
+        else{
+          document.getElementById("pp")!.style.display="block";
+        }
+    }
 }

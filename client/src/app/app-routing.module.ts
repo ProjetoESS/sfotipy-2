ef2ac678fff7c1606a11b9cbf2170907ff81b6dc
matheuslafayette <matheuslafayett@gmail.com>
2023-03-11 15:06:28 -0300

    criacao dos componentes básicos da tela de playlists recomendadas

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlaylistRecomendadasComponent } from './playlist-recomendadas/playlist-recomendadas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'playlistsrecomendadas', component: PlaylistRecomendadasComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlaylistRecomendadasComponent } from './playlist-recomendadas/playlist-recomendadas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlaylistRecomendadasComponent
   ],
   imports: [
     BrowserModule,

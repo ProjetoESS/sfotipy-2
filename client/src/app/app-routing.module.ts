1c5fabb02a037bc5de798d37584c00b0c84f6d07
Luiz Guylherme <86082879+Dollynt@users.noreply.github.com>
2023-03-21 20:50:50 -0300

    Protótipo front criar_playlist

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PlaylistComponent } from './componentes/criar-playlist/criar_playlist.component';
+
+const routes: Routes = [
+  { path: 'criar_playlist', component: PlaylistComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ client/src/app/componentes/criar-playlist/criar_playlist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './criar_playlist.component.html',
+  styleUrls: ['./criar_playlist.component.css']
+})
+export class PlaylistComponent implements OnInit {
+    constructor() {}
+    ngOnInit(): void {}
+}

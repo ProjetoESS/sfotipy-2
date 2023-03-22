f3facd98de601c03d7df3fa440c7338a4ecafa59
Luiz Guylherme <86082879+Dollynt@users.noreply.github.com>
2023-03-22 02:16:00 -0300

    adição da funcionalidade de importação da imagem da playlist

--- client/src/app/componentes/criar-playlist/criar_playlist.component.ts
+++ client/src/app/componentes/criar-playlist/criar_playlist.component.ts
@@ -9,9 +9,34 @@ import { OnInit } from '@angular/core';
 export class PlaylistComponent implements OnInit {
     constructor() {}
 
+    
     criarPlaylist() {
         console.log('bom')
     }
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+      const input_pic: HTMLElement | null = document.querySelector('.picture_input');
+      const pictureImage = document.querySelector('.picture_image')
+      if (input_pic && pictureImage){
+      input_pic.addEventListener('change', function(e){
+        const inputTarget = e.target as HTMLInputElement;
+        if (inputTarget.files){
+        const file = inputTarget.files[0];
+        const reader = new FileReader();
+        reader.addEventListener('load', function(e) {
+          const readerTarget = e.target
+          if (readerTarget && readerTarget.result) {
+          const img = document.createElement('img');
+          img.src = readerTarget.result as string
+          img.classList.add('picture_img')
+
+          pictureImage.innerHTML = '';
+          pictureImage.appendChild(img)
+          }
+        })
+        reader.readAsDataURL(file);
+        }
+        })
+      }
+    }
 }

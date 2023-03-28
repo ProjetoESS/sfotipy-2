96166fcd332b5525afa0610324e7392be71edecd
Angel Willyan Rodrigues de Lima <104532060+AngelWRlima@users.noreply.github.com>
2023-03-28 14:38:12 -0300

    Incorporação da tela de login base e dos assets no angular

--- cliente/src/app/login/login.component.spec.ts
+++ cliente/src/app/login/login.component.spec.ts
@@ -1,29 +1,29 @@
 import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { LoginComponent } from './login.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        LoginComponent
       ],
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(LoginComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'cliente'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(LoginComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('cliente');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(LoginComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('cliente app is running!');

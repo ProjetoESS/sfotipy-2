96b960e3e2e706f9660f3665b76b3b75a5493476
jefersonseverino <jsa2@cin.ufpe.br>
2023-03-28 18:35:45 -0300

    fix: apagando arquivos e trechos desnecessários

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,6 @@
 import express = require('express');
 import bodyParser = require("body-parser");
 
-import { CarService } from './src/cars-service';
-import { Car } from './src/car';
 import { MusicService } from './src/music-service';
 import { Music } from '../common/music';
 
@@ -17,48 +15,6 @@ var allowCrossDomain = function (req: any, res: any, next: any) {
 app.use(allowCrossDomain);
 
 app.use(bodyParser.json());
-
-// var carService: CarService = new CarService();
-
-// app.get('/cars', function (req, res) {
-//   const cars = carService.get();
-//   res.send(JSON.stringify(cars));
-// });
-
-// app.get('/cars/:id', function (req, res) {
-//   const id = req.params.id;
-//   const car = carService.getById(id);
-//   if (car) {
-//     res.send(car);
-//   } else {
-//     res.status(404).send({ message: `Car ${id} could not be found` });
-//   }
-// });
-
-// app.post('/cars', function (req: express.Request, res: express.Response) {
-//   const car: Car = <Car>req.body;
-//   try {
-//     const result = carService.add(car);
-//     if (result) {
-//       res.status(201).send(result);
-//     } else {
-//       res.status(403).send({ message: "Car list is full" });
-//     }
-//   } catch (err) {
-//     const { message } = err;
-//     res.status(400).send({ message })
-//   }
-// });
-
-// app.put('/cars', function (req: express.Request, res: express.Response) {
-//   const car: Car = <Car>req.body;
-//   const result = carService.update(car);
-//   if (result) {
-//     res.send(result);
-//   } else {
-//     res.status(404).send({ message: `Car ${car.id} could not be found.` });
-//   }
-// })
 
 var musicService: MusicService = new MusicService();
 
@@ -101,17 +57,6 @@ app.put('/musics', function (req: express.Request, res: express.Response) {
     res.status(404).send({ message: `Music ${music.id} could not be found.` });
   }
 });
-
-app.get('/playlist/category/:id', function (req: express.Request, res: express.Response) {
-  const playlistId : number = req.params.id;
-  const music = musicService.getById(playlistId);
-  const categories = music.categories;
-  if(categories) {
-    res.send(categories);
-  } else {
-    res.status(404).send({message : 'Playlist could not be found'});
-  }
-})
 
 var server = app.listen(3000, function () {
   console.log('Example app listening on port 3000!')

accc470b83663dd5da12c0ef46d43440c9c2bd9c
jefersonseverino <jsa2@cin.ufpe.br>
2023-03-29 21:20:55 -0300

    feat: criando requisições no service

--- client/src/app/playlist.service.ts
+++ client/src/app/playlist.service.ts
@@ -10,14 +10,41 @@ import { retry, map } from 'rxjs/operators';
 
 export class PlaylistService {
 
-  private headers = new HttpHeaders({'Content-Type': 'application/json'});
   private taUrl = 'http://localhost:3000/'
+
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
 
   constructor(private http : HttpClient) { }
 
-  getCategories(id: number) : Observable<string[] | null> {
+  getCategories(id: number){
     return this.http.get<string[]>(this.taUrl + "playlist/category/" + id)
-        .pipe( retry(2) )
+        .pipe( retry(2) );
+  }
+
+  getAllCategories() {
+    return this.http.get<string[]>(this.taUrl + "playlist/category")
+        .pipe(retry(2));
+  }
+
+  addNewCategory(id: number, category : string) {
+    return this.http.post(this.taUrl + "playlist/category/" + id, category, this.httpOptions)
+          .pipe(retry(2));
+  }
+
+  deleteCategory(id: number, categories: string) {
+    
+    const data = {
+      params:{
+        category:categories
+      }
+    };
+
+    return this.http.delete(this.taUrl + "playlist/category" + id, data)
+          .pipe(retry(2));
   }
 
 }

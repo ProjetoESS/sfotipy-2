dcfea382d56411c870184db15e9cf2d9afbfe22f
jefersonseverino <jsa2@cin.ufpe.br>
2023-03-30 17:33:28 -0300

    feat: integrando service com componente

--- client/src/app/playlist.service.ts
+++ client/src/app/playlist.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders  } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
 import { retry, map } from 'rxjs/operators';
 
 @Injectable({
@@ -11,6 +10,8 @@ import { retry, map } from 'rxjs/operators';
 export class PlaylistService {
 
   private taUrl = 'http://localhost:3000/'
+  private playlistCategories : string[] = [];
+  private allCategories: string[] = [];
 
   private httpOptions = {
     headers: new HttpHeaders({
@@ -21,13 +22,26 @@ export class PlaylistService {
   constructor(private http : HttpClient) { }
 
   getCategories(id: number){
-    return this.http.get<string[]>(this.taUrl + "playlist/category/" + id)
-        .pipe( retry(2) );
+    this.http.get<string[]>(this.taUrl + "playlist/category/" + id)
+        .pipe( 
+          map((res: any) => res)
+        ).subscribe(
+          (res: string[]) => {
+            this.playlistCategories = res;
+          }
+        );
+    return this.playlistCategories;
   }
 
   getAllCategories() {
-    return this.http.get<string[]>(this.taUrl + "playlist/category")
-        .pipe(retry(2));
+    this.http.get(this.taUrl + "playlist/category")
+        .pipe(
+          map((res: any) => res.categories)
+        ).subscribe(
+          (categories:string[]) => {
+            this.allCategories = categories;
+        })
+    return this.allCategories;
   }
 
   addNewCategory(id: number, category : string) {
@@ -35,11 +49,11 @@ export class PlaylistService {
           .pipe(retry(2));
   }
 
-  deleteCategory(id: number, categories: string) {
+  deleteCategory(id: number, category: string) {
     
     const data = {
       params:{
-        category:categories
+        category:category
       }
     };
 

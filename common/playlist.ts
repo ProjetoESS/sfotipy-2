f4bb3d59ab0b79c9dcccc60d3d89cf2b15ebb4bb
Júlia Dias <julia.dias@citi.org.br>
2023-04-02 00:24:53 -0300

    mend

--- common/playlist.ts
+++ common/playlist.ts
@@ -2,19 +2,23 @@ import { Music } from "./music";
 
 export class Playlist {
     id: number;
-    categories : string[];
+    categories: string[];
     musics: Music[];
+    image: string;
+    songs: Music[];
 
     constructor(playlist: Playlist) {
         this.id = playlist.id;
         this.categories = playlist.categories;
-        this.musics  = playlist.musics;
+        this.musics = playlist.musics;
+        this.image = "";
+        this.songs = [];
     }
 
     update(playlist: Playlist): void {
         this.id = playlist.id;
         this.categories = playlist.categories;
-        this.musics  = playlist.musics;
+        this.musics = playlist.musics;
     }
 
 }

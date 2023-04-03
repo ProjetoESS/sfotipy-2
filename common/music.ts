d78fe15d50b2a640a59b3499df9e77f99063cdaf
V1ct0rC <vgc3@cin.ufpe.br>
2023-04-03 17:50:07 -0300

    Readicionando music.ts

---
+++ common/music.ts
@@ -0,0 +1,22 @@
+export class Music {
+    id: number;
+    name: string;
+    author: string;
+    image : string;
+    link : string;
+    duration : string;
+
+    constructor(music: Music) {
+        this.id = music.id;
+        this.name = music.name;
+        this.author = music.author;
+        this.image = "";
+        this.link = "";
+        this.duration = "";
+    }
+
+    update(music: Music): void {
+        this.name = music.name;
+        this.author = music.author;
+    }
+}

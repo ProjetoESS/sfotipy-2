a2eb861a383dd3a7332147574caf38cc4562ae69
V1ct0rC <vgc3@cin.ufpe.br>
2023-04-03 17:33:22 -0300

    alterações de rotas e exclusão e códigos irrelevantes

--- client/src/app/homepage/homepage.component.ts
+++ client/src/app/homepage/homepage.component.ts
@@ -15,30 +15,10 @@ export class HomepageComponent {
   plRec : Playlist[] = [];
   plMin : Playlist[] = [];
 
-  /*
-  plEma : Array<[string, string]>;
-  plPub : Array<[string, string]>;
-  plRec : Array<[string, string]>;
-  plMin : Array<[string, string]>;
-  */
-
   @Input() isLogged : boolean = true;
 
   constructor(private playlistService : PlaylistService){
-    //Temporário
-    /*
-    this.plEma = [["plst1", "/assets/cover9.jpg"], ["plst2", "/assets/cover3.webp"], ["plst3", "/assets/cover5.webp"], 
-                  ["plst4", "/assets/cover1.jpg"], ["plst5", "/assets/cover2.jpg"]]
-
-    this.plPub = [["plst1", "/assets/cover1.jpg"], ["plst2", "/assets/cover2.jpg"], ["plst3", "/assets/cover3.webp"], 
-                  ["plst4", "/assets/cover4.webp"], ["plst5", "/assets/cover5.webp"]];
-
-    this.plRec = [["plst1", "/assets/cover6.jpg"], ["plst2", "/assets/cover7.jpg"], ["plst3", "/assets/cover8.jpg"], 
-                  ["plst4", "/assets/cover9.jpg"], ["plst5", "/assets/cover1.jpg"]];
-
-    this.plMin = [["plst1", "/assets/cover2.jpg"], ["plst2", "/assets/cover3.webp"], ["plst3", "/assets/cover4.webp"], 
-                  ["plst4", "/assets/cover5.webp"], ["plst5", "/assets/cover6.jpg"]];
-    */
+    
   }
 
   ngOnInit(): void {

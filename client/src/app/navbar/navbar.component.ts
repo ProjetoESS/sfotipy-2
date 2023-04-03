3bc0cb8a4ac9b4c167757940ca35acd4e6f6e090
V1ct0rC <vgc3@cin.ufpe.br>
2023-04-03 18:38:19 -0300

    Realocação do componente profilepage

--- client/src/app/navbar/navbar.component.ts
+++ client/src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  show : boolean = false;
+  @Input() isLogged : boolean = true;
+  @Output() logOutEvent = new EventEmitter<boolean>();	
+  
+  showProfile() : void{
+    console.log(this.show);
+    
+    this.show = !this.show;
+  }
+  
+	getLogOut($event : boolean) : void{
+		this.isLogged = $event
+		document.getElementById("pp")!.style.display="none";
 
+    this.logOutEvent.emit(this.isLogged);
+	}
 }

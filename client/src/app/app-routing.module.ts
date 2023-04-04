62000de6ae3ca5860a799462ce878e9d6d1cd0f9
Luiz Guylherme <86082879+Dollynt@users.noreply.github.com>
2023-04-04 20:47:14 -0300

    correção diretórios para integração

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { CriarPlaylistComponent } from './componentes/criar-playlist/criar_playlist.component';
-import { UserPlaylistsComponent } from './componentes/user-playlists/user_playlists.component';
+import { CriarPlaylistComponent } from './criar-playlist/criar_playlist.component';
+import { UserPlaylistsComponent } from './user-playlists/user_playlists.component';
 
 const routes: Routes = [
   { path: 'criar_playlist', component: CriarPlaylistComponent},

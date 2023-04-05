eb475a9faeedfa80158c863ac783b15ea0fdf545
Luiz Guylherme <86082879+Dollynt@users.noreply.github.com>
2023-04-05 03:06:23 -0300

    exibição das músicas da playlist

--- client/src/app/playlist/playlist.component.ts
+++ client/src/app/playlist/playlist.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PlaylistModule } from "./playlists.module";
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,39 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class PlaylistComponent implements OnInit {
     constructor(private route: ActivatedRoute) {}
+
+    songs: any[] = [
+      {
+        id: 1,
+        name: "Música 1",
+        artist: 'musico1'
+      },
+      {
+        id: 2,
+        name: "Música 2",
+        artist: 'musico2'
+      },
+      {
+        id: 3,
+        name: "Música 3",
+        artist: 'musico3'
+      },
+      {
+        id: 4,
+        name: "Música 4",
+        artist: 'musico4'
+      },
+      {
+        id: 5,
+        name: "Música 5",
+        artist: 'musico5'
+      },
+      {
+        id: 6,
+        name: "Música 6",
+        artist: 'musico6'
+      }
+    ]
 
     playlists: any[] = [ // Array com informações das playlists
     {
@@ -33,12 +67,14 @@ export class PlaylistComponent implements OnInit {
     }
 
     selectedPlaylist: any; // Propriedade que receberá a playlist selecionada
+    playlistSongs: any[] = [];
 
     ngOnInit(): void {
       this.route.paramMap.subscribe(params => {
         if(params && params.get('id')) {
           const id = params?.get('id');// Obtém o valor do parâmetro id da rota e converte para número
           this.selectedPlaylist = this.playlists.find(playlist => playlist.id == id); // Procura a playlist correspondente ao id na lista de playlists
+          this.playlistSongs = this.songs.filter(song => this.selectedPlaylist.songs.includes(song.name));
         }
       });
     }

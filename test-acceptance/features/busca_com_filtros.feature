538095de615fa7c9b11a658cb822c3bb3e7aa8f2
Júlia Dias <julia.dias@citi.org.br>
2023-04-06 00:23:22 -0300

    Primeiro teste em busca

--- test-acceptance/features/busca_com_filtros.feature
+++ test-acceptance/features/busca_com_filtros.feature
@@ -6,5 +6,5 @@ Feature: Busca com filtros
 Scenario: Busca com Filtro por trecho do nome
 Given eu estou na página "busca"
 And as músicas "Yellow", "Viva la vida", "Paradise" e "Another day in Paradise" aparecem na lista de músicas
-When eu escrever um "Paradise" na busca por texto
-Then eu posso ver as músicas "Paradise" e "Another day in Paradise" aparecem na lista de músicas
+When eu preencher o campo de busca por texto com "Paradise"
+Then eu posso ver as músicas "Paradise" e "Another day in Paradise" na lista de músicas
---
+++ test-acceptance/stepdefinitions/busca.ts
@@ -0,0 +1,36 @@
+import { defineSupportCode } from 'cucumber';
+import { browser, $, element, ElementArrayFinder, by } from 'protractor';
+let chai = require('chai').use(require('chai-as-promised'));
+let expect = chai.expect;
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+defineSupportCode(function ({ Given, When, Then }) {
+    Given(/^eu estou na página "([^\"]*)"$/, async (name) => {
+        await browser.get("http://localhost:4200/busca");
+        await expect(browser.getTitle()).to.eventually.equal('Sfotipy');
+    })
+
+    Given(/^as músicas "([^\"]*)", "([^\"]*)", "([^\"]*)" e "([^\"]*)" aparecem na lista de músicas$/,
+        async (music1, music2, music3, music4) => {
+            expect((await element.all(by.id(`music-${music1}`))).length).to.equal(1);
+            expect((await element.all(by.id(`music-${music2}`))).length).to.equal(1);
+            expect((await element.all(by.id(`music-${music3}`))).length).to.equal(1);
+            expect((await element.all(by.id(`music-${music4}`))).length).to.equal(1);
+        });
+
+    When(/^eu preencher o campo de busca por texto com "([^\"]*)"$/,
+        async (name) => {
+            await element(by.id("text-search-input")).clear();
+
+            await element(by.id("text-search-input")).sendKeys(<string>name);
+        })
+
+    Then(/^eu posso ver as músicas "([^\"]*)" e "([^\"]*)" na lista de músicas$/,
+        async (music1, music2) => {
+            expect((await element.all(by.id(`music-${music1}`))).length).to.equal(1);
+            expect((await element.all(by.id(`music-${music2}`))).length).to.equal(1);
+        })
+})

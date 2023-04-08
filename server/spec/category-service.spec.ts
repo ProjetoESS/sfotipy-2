69ae56e0d544d0ff64a0cdac2ee032af3da5afa0
jefersonseverino <jsa2@cin.ufpe.br>
2023-04-08 15:55:40 -0300

    feat: creating test files and update jasmine

---
+++ server/spec/category-service.spec.ts
@@ -0,0 +1,38 @@
+import 'jasmine';
+import { Category } from '../../common/category';
+import { CategoryService } from '../src/category-service';
+
+describe("O servico de categorias", () => {
+    var categoryService : CategoryService;
+
+    beforeEach(() => categoryService = new CategoryService())
+
+    it("Espera que existam 6 categorias", () => {
+        expect(categoryService.get().length).toBe(6);
+    });
+
+    it("Espera que a categoria de id 1 seja Pop", () => {
+        expect(categoryService.getById(1).name).toBe("Pop");
+    });
+
+    it("Espera que a categoria de id 2 seja Rock", () => {
+        expect(categoryService.getById(2).name).toBe("Rock");
+    });
+
+    it("Espera que a categoria de id 3 seja Electronic", () => {
+        expect(categoryService.getById(3).name).toBe("Electronic");
+    });
+
+    it("Espera que a categoria de id 4 seja Hip-Hop", () => {
+        expect(categoryService.getById(4).name).toBe("Hip-Hop");
+    });
+
+    it("Espera que a categoria de id 5 seja KPop", () => {
+        expect(categoryService.getById(5).name).toBe("KPop");
+    });
+
+    it("Espera que a categoria de id 6 seja Indie", () => {
+        expect(categoryService.getById(6).name).toBe("Indie");
+    });
+
+});
---
+++ server/spec/music-service.spec.ts
@@ -0,0 +1,12 @@
+import 'jasmine'
+import { MusicService } from '../src/music-service'
+import { Music } from '../../common/music'
+
+describe('MusicService', () => {
+
+    var musicService : MusicService;
+    beforeEach(() => musicService = new MusicService());
+
+    
+
+});

012a36b0dafa345301be51baff69842152d674c5
V1ct0rC <vgc3@cin.ufpe.br>
2023-04-08 14:42:30 -0300

    fix: recreating playlists

--- server/src/playlist-service.ts
+++ server/src/playlist-service.ts
@@ -64,27 +64,6 @@ export class PlaylistService {
       'availability': 'public'
     }),
     new Playlist(<Playlist>{
-      "id": 3,
-      "name": "League of Legends Mix",
-      "categories": [],
-      "musics": [18, 19, 20],
-      "image": "https://cdns-images.dzcdn.net/images/artist/21e53b8e8285f84f60601d895c39c900/500x500.jpg",
-      "link": "",
-      "owner": "",
-      "followers": [],
-      "availability": "public"
-    }),
-    new Playlist(<Playlist>{
-      "id": 4,
-      "name": "The NBHD",
-      "categories": [],
-      "musics": [21, 22, 23, 24],
-      "image": "https://i.pinimg.com/564x/78/35/fb/7835fb0bef03a3332c89c681f020da87--music-bands-.jpg",
-      "link": "",
-      "owner": "",
-      "followers": [],
-      "availability": "public"
-    })
       'id': 5,
       'name': 'Melhores Rock',
       'categories': [2],
@@ -96,6 +75,50 @@ export class PlaylistService {
       'followers': [],
       'availability': 'public'
     }),
+    new Playlist(<Playlist>{
+      "id": 6,
+      "name": "Rocking with Imagine Dragons",
+      "categories": [],
+      "musics": [10, 11, 12, 13, 14],
+      "image": "https://i.pinimg.com/736x/98/e6/d8/98e6d8ab4d4414eef0e90bb1382bfb86.jpg",
+      "link": "",
+      "owner": "",
+      "followers": [],
+      "availability": "public"
+    }),
+    new Playlist(<Playlist>{
+      "id": 7,
+      "name": "Eminem's Greatest Hits",
+      "categories": [],
+      "musics": [15, 16, 17],
+      "image": "https://images.fineartamerica.com/images/artworkimages/mediumlarge/3/eminem-miracle-studio.jpg",
+      "link": "",
+      "owner": "",
+      "followers": [],
+      "availability": "public"
+    }),
+    new Playlist(<Playlist>{
+      "id": 8,
+      "name": "League of Legends Mix",
+      "categories": [],
+      "musics": [18, 19, 20],
+      "image": "https://cdns-images.dzcdn.net/images/artist/21e53b8e8285f84f60601d895c39c900/500x500.jpg",
+      "link": "",
+      "owner": "",
+      "followers": [],
+      "availability": "public"
+    }),
+    new Playlist(<Playlist>{
+      "id": 9,
+      "name": "The NBHD",
+      "categories": [],
+      "musics": [21, 22, 23, 24],
+      "image": "https://i.pinimg.com/564x/78/35/fb/7835fb0bef03a3332c89c681f020da87--music-bands-.jpg",
+      "link": "",
+      "owner": "",
+      "followers": [],
+      "availability": "public"
+    })
   ];
 
   idCount: number = 6;
@@ -147,13 +170,6 @@ export class PlaylistService {
 
   getById(playlistId: number): Playlist | undefined {
     return this.playlists.find(({ id }) => id == playlistId);
-  }
-
-  add(playlist: Playlist): Playlist {
-    const newPlaylist = new Playlist(<Playlist>{ ...playlist, id: this.idCount });
-    this.playlists.push(newPlaylist);
-    this.idCount++;
-    return newPlaylist;
   }
 
   update(playlist: Playlist): Playlist|null {

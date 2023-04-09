34a2dcc32a83c27a4ee20e2fed479cc7c133f1fb
jefersonseverino <jsa2@cin.ufpe.br>
2023-04-09 10:59:47 -0300

    refactor: melhorar legibilidade de testes

--- server/spec/user-service.spec.ts
+++ server/spec/user-service.spec.ts
@@ -5,6 +5,14 @@ import { User } from '../../common/user'
 describe('UserService', () => {
     var userService : UserService;
 
+    beforeAll(() => {
+        process.stdout.write("user-service: ");
+    });
+
     beforeEach(() => userService = new UserService());
+
+    afterAll(() => {
+        console.log("\n");
+    });
 
 });

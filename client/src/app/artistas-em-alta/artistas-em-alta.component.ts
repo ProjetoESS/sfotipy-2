62916badaa4a70e761a4cfaa874490393c0904d3
eduarda09 <m.eduardakl@gmail.com>
2023-04-08 23:41:30 -0300

    feature(feature/artistasEmAlta): created the function to show up the top artists

--- client/src/app/artistas-em-alta/artistas-em-alta.component.ts
+++ client/src/app/artistas-em-alta/artistas-em-alta.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Music } from '../../../../common/music';
+import { MusicService } from '../../../../server/src/music-service';
 
 @Component({
   selector: 'app-artistas-em-alta',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./artistas-em-alta.component.scss']
 })
 export class ArtistasEmAltaComponent {
+  mostAccessedArtists: Music[] = [];
+  constructor(private musicService: MusicService) { }
 
+  ngOnInit() {
+    this.mostAccessedArtists = this.musicService.getMostAccessed();
+
+    // const newList: Music[] = [];
+
+    // for (let i = 0; i < this.mostAccessedArtists.length; i++) {
+    //   newList.push(this.mostAccessedArtists[i]);
+    // }
+
+  }
 }

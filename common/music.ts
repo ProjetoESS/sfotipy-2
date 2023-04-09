d78ad693001931623f9768ccfd0a8cfec02a9f46
eduarda09 <m.eduardakl@gmail.com>
2023-04-09 17:27:37 -0300

    modified:   ../common/music.ts 	modified:   ../common/playlist.ts

--- common/music.ts
+++ common/music.ts
@@ -1,17 +1,13 @@
 export class Music {
-    id: number;
-    name: string;
-    author: string;
+    id : number;
+    name : string;
+    image : string;
+    link : string;
 
-    constructor(music: Music) {
-        this.id = music.id;
-        this.name = music.name;
-        this.author = music.author;
+    constructor() {
+        this.id = 0;
+        this.name = "";
+        this.image = "";
+        this.link = "";
     }
-
-    update(music: Music): void {
-        this.name = music.name;
-        this.author = music.author;
-    }
-
 }

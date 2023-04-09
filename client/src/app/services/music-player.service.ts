d24ae43249dd843396c859ea0562378d2468dac1
Júlia Dias <julia.dias@citi.org.br>
2023-04-09 04:47:17 -0300

    feat: play musics from a list, next and back functions

--- client/src/app/services/music-player.service.ts
+++ client/src/app/services/music-player.service.ts
@@ -6,7 +6,16 @@ import { Music } from '../../../../common/music';
 })
 export class MusicPlayerService {
 
-  currentMusic: Music = new Music(<Music>{ 'id': 0, 'name': "Hymn for the Weekend", 'author': "Coldplay", 'image': "https://upload.wikimedia.org/wikipedia/pt/6/61/Coldplay_Hymn_for_the_Weekend.jpg", 'link': "", 'duration': 500, 'category': 3 });
+  playlist: Music[] = [
+    new Music(<Music>{ 'id': 0, 'name': "Hymn for the Weekend", 'author': "Coldplay", 'image': "https://upload.wikimedia.org/wikipedia/pt/6/61/Coldplay_Hymn_for_the_Weekend.jpg", 'link': "", 'duration': 500, 'category': 3 }),
+    new Music(<Music>{ 'id': 1, 'name': "Viva la Vida", 'author': "Coldplay", 'image': "https://upload.wikimedia.org/wikipedia/pt/d/d7/Vivalavida.jpg", 'link': "", 'duration': 500, 'category': 2 }),
+    new Music(<Music>{ 'id': 2, 'name': "Yellow", 'author': "Coldplay", 'image': "https://i.pinimg.com/originals/f7/30/23/f7302371fb79c2d0cd817e9c28baaf62.jpg", 'link': "", 'duration': 500, 'category': 3 }),
+    new Music(<Music>{ 'id': 3, 'name': "Paradise", 'author': "Coldplay", 'image': "https://cdns-images.dzcdn.net/images/cover/e0dd8263dfed37c50a868abbf65fd7da/500x500.jpg", 'link': "", 'duration': 500, 'category': 1 })
+  ];
+
+  currentIndex: number = 0;
+
+  currentMusic: Music = this.playlist[this.currentIndex];
 
   audio: HTMLAudioElement = new Audio('assets/musics/' + this.currentMusic.id + '/audio.mp3');
 
@@ -22,6 +31,24 @@ export class MusicPlayerService {
     this.updateInterval = setInterval(() => {
       this.updateMusicInfo();
     }, 1000);
+
+    this.audio.addEventListener('ended', () => {
+      this.next();
+    });
+  }
+
+  next() {
+    if (this.currentIndex < this.playlist.length - 1) {
+      this.playMusic(this.playlist[this.currentIndex + 1]);
+    } else {
+      this.playMusic(this.playlist[0]);
+    }
+  }
+
+  back() {
+    if (this.currentIndex > 0) {
+      this.playMusic(this.playlist[this.currentIndex - 1]);
+    }
   }
 
   updateMusicInfo() {
@@ -31,6 +58,7 @@ export class MusicPlayerService {
   }
 
   playMusic(music: Music) {
+    this.currentIndex = this.playlist.indexOf(music);
     this.currentMusic = music;
     this.audio.pause();
     this.audio.src = 'assets/musics/' + this.currentMusic.id + '/audio.mp3';

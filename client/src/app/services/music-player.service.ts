11e4cf79bb352b8e792080fcae1b18357a5ae505
Júlia Dias <julia.dias@citi.org.br>
2023-04-09 03:28:52 -0300

    feat: play songs from service

--- client/src/app/services/music-player.service.ts
+++ client/src/app/services/music-player.service.ts
@@ -7,6 +7,9 @@ import { Music } from '../../../../common/music';
 export class MusicPlayerService {
 
   currentMusic: Music = new Music(<Music>{ 'id': 0, 'name': "Hymn for the Weekend", 'author': "Coldplay", 'image': "https://upload.wikimedia.org/wikipedia/pt/6/61/Coldplay_Hymn_for_the_Weekend.jpg", 'link': "", 'duration': 500, 'category': 3 });
+
+  audio: HTMLAudioElement = new Audio('assets/musics/' + this.currentMusic.id + '/audio.mp3');
+
   isPlaying: boolean = false;
 
   musicTime = { 'minutes': Math.floor(this.currentMusic.duration / 60), 'seconds': this.currentMusic.duration - Math.floor(this.currentMusic.duration / 60) * 60 }
@@ -18,10 +21,12 @@ export class MusicPlayerService {
   playMusic(music: Music) { }
 
   play() {
+    this.audio.play();
     this.isPlaying = true;
   }
 
   pause() {
+    this.audio.pause();
     this.isPlaying = false;
   }
 

76a3e6c22dbe61bf507c66b8cd3d28ca4b563d13
V1ct0rC <vgc3@cin.ufpe.br>
2023-04-10 16:14:45 -0300

    fix: tets almost complete

--- client/src/app/pages/homepage/homepage.component.ts
+++ client/src/app/pages/homepage/homepage.component.ts
@@ -40,7 +40,7 @@ export class HomepageComponent {
               );
 
               this.playlistsPublic = this.playlists.filter((playlist) => playlist.availability === "public"); 
-              this.playlistsUser = this.playlists.filter(playlist => playlist.ownerId == this.userId);
+              this.playlistsUser = this.playlists.filter(playlist => playlist.ownerId === this.userId);
               this.playlistsTrending = this.playlists.sort((a, b) => b.followers.length - a.followers.length).splice(0,5);
 
               //this.playlistsRecommended = this.playlistsPublic.sort((a, b) => b.followers.length - a.followers.length).slice(0, 3);

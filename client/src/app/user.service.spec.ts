b96c06fdd9e02c8249983b129cc0ab55fd7956d7
Júlia Dias <julia.dias@citi.org.br>
2023-04-10 20:25:47 -0300

    fix: detatch test from server

--- client/src/app/user.service.spec.ts
+++ client/src/app/user.service.spec.ts
@@ -2,19 +2,22 @@ import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
 import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { User } from '../../../common/user';
 
 describe('UserService', () => {
   let service: UserService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule]
+      imports: [HttpClientTestingModule, HttpClientModule, HttpClientModule]
     });
     service = TestBed.inject(UserService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
-
 });

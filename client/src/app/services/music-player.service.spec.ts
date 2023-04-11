f9109c444f94c00e86e3d937c966118f495b03b7
Júlia Dias <julia.dias@citi.org.br>
2023-04-10 23:32:40 -0300

    test(music-player): play playlist

--- client/src/app/services/music-player.service.spec.ts
+++ client/src/app/services/music-player.service.spec.ts
@@ -3,9 +3,29 @@ import { TestBed } from '@angular/core/testing';
 import { MusicPlayerService } from './music-player.service';
 import { HttpClientModule } from '@angular/common/http';
 import { Music } from '../../../../common/music';
+import { Playlist } from '../../../../common/playlist';
 
 describe('MusicPlayerService', () => {
   let service: MusicPlayerService;
+  let music: Music;
+  let playlist: Playlist;
+
+  beforeAll(() => {
+    music = new Music(<Music>{ 'id': 0, 'name': "Hymn for the Weekend", 'author': "Coldplay", 'image': "https://upload.wikimedia.org/wikipedia/pt/6/61/Coldplay_Hymn_for_the_Weekend.jpg", 'link': "", 'duration': 500, 'category': 3 });
+    playlist = new Playlist(<Playlist><unknown>{
+      "id": 0,
+      "name": "Mix de Coldplay",
+      "categories": [1],
+      "musics": [0, 1, 2, 3, 4, 5, 6, 7, 8],
+      "image": "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/2f679c136425765.61f96b4f03c85.jpg",
+      "link": "",
+      "owner": "João",
+      "followers": ['a', 'b'],
+      "availability": "public",
+      "accessPlaylits": 4,
+      "ownerId": 1
+    })
+  });
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -19,7 +39,6 @@ describe('MusicPlayerService', () => {
   });
 
   it('should play music', () => {
-    const music: Music = new Music(<Music>{ 'id': 0, 'name': "Hymn for the Weekend", 'author': "Coldplay", 'image': "https://upload.wikimedia.org/wikipedia/pt/6/61/Coldplay_Hymn_for_the_Weekend.jpg", 'link': "", 'duration': 500, 'category': 3 });
     spyOn(service, 'play');
     service.playMusic(music);
     expect(service.currentMusic).toEqual(music);
@@ -33,4 +52,12 @@ describe('MusicPlayerService', () => {
     expect(service.pause).toHaveBeenCalled();
   });
 
+  it('should play playlist', async () => {
+    spyOn(service, 'play');
+    service.playPlaylist(playlist);
+    await new Promise(resolve => setTimeout(resolve, 1000))
+    expect(service.playlist).toEqual(playlist);
+    expect(service.currentMusic.id).toEqual(playlist.musics[0]);
+    expect(service.play).toHaveBeenCalled();
+  });
 });

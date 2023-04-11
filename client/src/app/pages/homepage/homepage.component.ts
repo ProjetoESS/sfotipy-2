3c2dc18ee590f16ffdde3cae884915b8e764ad20
V1ct0rC <vgc3@cin.ufpe.br>
2023-04-11 20:43:35 -0300

    fix: number of trending playlists

--- client/src/app/pages/homepage/homepage.component.ts
+++ client/src/app/pages/homepage/homepage.component.ts
@@ -42,7 +42,7 @@ export class HomepageComponent {
               this.playlistsPublic = this.playlists.filter((playlist) => playlist.availability === "public"); 
               this.playlistsUser = this.playlists.filter(playlist => playlist.ownerId === this.userId);
               //this.playlistsTrending = this.playlists.sort((a, b) => b.followers.length - a.followers.length).splice(0,4);
-              this.playlistsTrending = this.playlistsPublic.sort((a, b) => b.accessPlaylits - a.accessPlaylits).slice(0, 4);
+              this.playlistsTrending = this.playlistsPublic.sort((a, b) => b.accessPlaylits - a.accessPlaylits).slice(0, 5);
               
               this.playlistService.recommendPlaylists(this.playlistsUser, this.playlists).subscribe(
                 as => { this.playlistsRecommended = as; },

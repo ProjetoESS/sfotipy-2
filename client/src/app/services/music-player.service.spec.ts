028d7c3cabe42fe8d53bc4de1845c8fec08768ba
Júlia Dias <julia.dias@citi.org.br>
2023-04-10 21:35:48 -0300

    test(music-player): testing play music

--- client/src/app/services/music-player.service.spec.ts
+++ client/src/app/services/music-player.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { MusicPlayerService } from './music-player.service';
 import { HttpClientModule } from '@angular/common/http';
+import { Music } from '../../../../common/music';
 
 describe('MusicPlayerService', () => {
   let service: MusicPlayerService;
@@ -15,5 +16,13 @@ describe('MusicPlayerService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should play music', () => {
+    const music: Music = new Music(<Music>{ 'id': 0, 'name': "Hymn for the Weekend", 'author': "Coldplay", 'image': "https://upload.wikimedia.org/wikipedia/pt/6/61/Coldplay_Hymn_for_the_Weekend.jpg", 'link': "", 'duration': 500, 'category': 3 });
+    spyOn(service, 'play');
+    service.playMusic(music);
+    expect(service.currentMusic).toEqual(music);
+    expect(service.play).toHaveBeenCalled();
   });
 });
